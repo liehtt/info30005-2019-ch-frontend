a28de1e80821c93b57d18ac940ea33082fae503a
liehtt <liehtt@gmail.com>
2019-04-28 13:24:32 +1000

    fixed bug in userpage.js

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -4,25 +4,25 @@ import LeftContent from "./LeftContent";
 import axios from "axios";
 
 class UserPage extends Component {
-  state = {
-    currUser: {}
-  };
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      currUser: {}
+    };
   }
 
-  async componentDidMount() {
-    const { data: currUser } = await axios.get(
-      "https://info30005-2019-ch.herokuapp.com/api/user/0"
-    );
-    this.setState({ currUser });
-    //console.log(currUser);
+  componentDidMount() {
+    axios.get("https://info30005-2019-ch.herokuapp.com/api/user/0")
+        .then(res => {
+            this.setState({currUser: res.data});
+        })
   }
 
   render() {
     return (
-      <div className="user_page">
+      <div className="user-page">
         <Header user={this.state.currUser} />
         <LeftContent user={this.state.currUser} />
       </div>
