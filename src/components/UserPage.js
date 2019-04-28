60d15c43197f30d5a8811558e68681df7e749aa5
liehtt <liehtt@gmail.com>
2019-04-28 16:21:17 +1000

    added comments

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,33 +1,21 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import UserContent from "./UserContent";
-import axios from "axios";
 
-class UserPage extends Component {
+// the parent component of userpage
+export default class UserPage extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      currUser: {}
-    };
   }
 
-  componentDidMount() {
-    axios.get("https://info30005-2019-ch.herokuapp.com/api/user/0")
-        .then(res => {
-            this.setState({currUser: res.data});
-        })
-  }
-
+  // renders header and user-content
   render() {
     return (
       <div className="user-page">
-        <Header user={this.state.currUser} />
-        <UserContent user={this.state.currUser} />
+        <Header  />
+        <UserContent />
       </div>
     );
   }
 }
-
-export default UserPage;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import UserPage from './components/UserPage';
 //import LoginPage from './components/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+// renders UserPage and the only ReactDOM.render, connects to ./public/index.html
 ReactDOM.render(<UserPage/>, document.getElementById('root'));
