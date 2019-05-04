3991c4d5a509527599c811b7cac009e2ddd6523d
liehtt <liehtt@gmail.com>
2019-05-05 03:54:48 +1000

    added App.js for central React component that links React pages

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import UserPage from './UserPage';
+import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showUserPage: false,
+            showLoginPage: false,
+            showSignUpPage: true
+        }
+
+        this.triggerLoginPage = this.triggerLoginPage.bind(this);
+        this.triggerUserPage = this.triggerUserPage.bind(this);
+
+    }
+
+    triggerLoginPage() {
+        this.setState({
+            showLoginPage: true,
+            showUserPage: false,
+            showSignUpPage: false
+        });
+    }
+
+    triggerUserPage() {
+        this.setState({
+            showLoginPage: false,
+            showUserPage: true,
+            showSignUpPage: false
+        })
+    }
+
+    render() {
+        if(this.state.showUserPage) {
+            return (<UserPage />);
+        } else if(this.state.showLoginPage) {
+            return (<LoginPage func={this.triggerUserPage} />);
+        } else if(this.state.showSignUpPage) {
+            return (<SignUpPage func={this.triggerLoginPage}/>);
+        }
+    }
+}
