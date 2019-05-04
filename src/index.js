d1b110fb039d54ba6492ac1ea97fbadd8ab5d978
liehtt <liehtt@gmail.com>
2019-05-04 17:00:59 +1000

    Added loginpage from unused folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UserPage from './components/UserPage';
-//import LoginPage from './components/LoginPage';
+import LoginPage from './components/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // renders UserPage and the only ReactDOM.render, connects to ./public/index.html
