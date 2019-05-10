4b77ca0eea4043eb323fafa50cac7e41f3e2da29
John Stephenson <jgbstephenson@gmail.com>
2019-05-11 04:07:20 +1000

    added personal club listing functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { O_RDWR } from "constants";
 
 // renders UserPage and the only ReactDOM.render, connects to ./public/index.html
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />,
+  </BrowserRouter>,
+  document.getElementById("root")
+);
