9723a432c141f07c839ef95b845da0377be09bb0
Ujashkumar Patel <upatel@student.unimelb.edu.au>
2019-05-22 23:55:20 +1000

    fixed the nav bar a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav, Button, NavDropdown } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 import "./custom.css";
 import Api from './Api'
+import logo from '../images/border-logo.svg'
 
 // child component of UserPage
 export default class Header extends Component {
@@ -32,21 +33,40 @@ export default class Header extends Component {
      render() {
         return (
           <div>
-            <Navbar fixed="top" className="navbar" variant="dark" expand="lg">
-              <Navbar.Brand className="navbar-brand" href="/">
-                Clubhub
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <NavLink to='/' activeClassName='link'>Dashboard</NavLink>
-                <NavLink to='/' activeClassName='link'>Notifications</NavLink>
-                <NavLink to='/clubs' activeClassName='link'>Browse Clubs</NavLink>
-                <NavLink to='/' activeClassName='link'>Browse Events</NavLink>
-                <NavLink to='/' activeClassName='link'>{this.state.user.username}</NavLink>
-                <Button variant='outline-primary' onClick={this.handleClick}>Log Out</Button>
+            {/*<Navbar fixed="top" className="navbar" variant="dark" expand="lg">*/}
+              {/*<Navbar.Brand  href="/">*/}
+                {/*<img className="navbar-brand" src={logo} alt="Clubhub Brand Logo"/>*/}
+              {/*</Navbar.Brand>*/}
+              {/*<Navbar.Toggle aria-controls="basic-navbar-nav" />*/}
+              {/*<Navbar.Collapse id="basic-navbar-nav">*/}
+               {/*<NavLink to='/' activeClassName='link'>Dashboard</NavLink>*/}
+                {/*<NavLink to='/' activeClassName='link'>Notifications</NavLink>*/}
+                {/*<NavLink to='/clubs' activeClassName='link'>Browse Clubs</NavLink>*/}
+                {/*<NavLink to='/' activeClassName='link'>Browse Events</NavLink>*/}
+                {/*<NavLink to='/' activeClassName='link'>{this.state.user.username}</NavLink>*/}
+                {/*<Button variant='outline-primary' onClick={this.handleClick}>Log Out</Button>*/}
 
-              </Navbar.Collapse>
-            </Navbar>
+              {/*</Navbar.Collapse>*/}
+            {/*</Navbar>*/}
+
+              <Navbar className="navbar" fixed="top" collapseOnSelect expand="lg" >
+                  <Navbar.Brand href="#home">
+                      <img className="navbar-brand" src={logo} alt="Clubhub Brand Logo"/>
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                      <Nav>
+                          <NavLink to='/' activeClassName='link'>Dashboard</NavLink>
+                          <NavLink to='/' activeClassName='link'>Notifications</NavLink>
+                          <NavLink to='/clubs' activeClassName='link'>Browse Clubs</NavLink>
+                          <NavLink to='/' activeClassName='link'>Browse Events</NavLink>
+                      </Nav>
+                      <Nav className="ml-auto">
+                          <NavLink to='/' activeClassName='link'> Hi, {this.state.user.username}</NavLink>
+                          <Button className="custom-purple-outline-btn" onClick={this.handleClick}>Log Out</Button>
+                      </Nav>
+                  </Navbar.Collapse>
+              </Navbar>;
           </div>
         )
       }
