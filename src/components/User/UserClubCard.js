844b3579ed880a4e8b49c778965cc1dd7b725832
liehtt <liehtt@gmail.com>
2019-06-01 21:34:44 +1000

    added leave button

--- src/components/User/UserClubCard.js
+++ src/components/User/UserClubCard.js
@@ -2,12 +2,38 @@ import React, { Component } from "react";
 import {
     Card, Col, Button, ListGroup
 } from "react-bootstrap";
+import Api from '../Api';
 
 export default class UserClubCard extends Component {
 
   constructor(props) {
     super(props);
 
+    this.state = {
+      toggleClick: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.removeClub = this.removeClub.bind(this);
+  }
+
+  async removeClub() {
+    const {data: user} = await Api.get('/api/user/profile');
+    const userId = user._id;
+    const clubId = this.props.thisClub._id;
+    const rtr = await Api.post('/api/user/removeClub', {
+      clubId: clubId,
+      userId: userId
+    });
+    const rtn2 = await Api.post('/api/club/removeMember', {
+      clubId: clubId,
+      userId: userId
+    });
+  }
+
+  handleClick() {
+    this.setState({toggleClick: true});
+    this.removeClub();
   }
 
   render() {
@@ -18,6 +44,13 @@ export default class UserClubCard extends Component {
           <Card bg="light" className="user-club-card">
             <Card.Body>
               <Card.Title>{club.clubname}</Card.Title>
+              <Card.Body>
+                <div className="toggle-button">
+                  <Button className="custom-purple-filled-btn" variant="info" onClick={this.handleClick} block>
+                    {this.state.toggleClick ? "How dare you!" : "Leave Club"}
+                  </Button>
+                </div>
+              </Card.Body>
             </Card.Body>
           </Card>
         </Col>
