30cdabe194418ab5fbe421ce35b1c205dda14b96
liehtt <liehtt@gmail.com>
2019-06-01 20:10:55 +1000

    simple members list in clubpage

--- src/components/ClubHeader.js
+++ src/components/ClubHeader.js
@@ -41,9 +41,8 @@ export default class ClubHeader extends Component {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav>
                             <NavLink to='/club/profile' activeClassName='link'>Dashboard</NavLink>
-                            <NavLink to='/' activeClassName='link'>Events</NavLink>
-                            <NavLink to='/clubs' activeClassName='link'>Members</NavLink>
-
+                            <NavLink to='/club/profile' activeClassName='link'>Events</NavLink>
+                            <NavLink to='/club/members' activeClassName='link'>Members</NavLink>
                         </Nav>
                         <Nav className="ml-auto">
                             {/*<img className="user-turtle-img" src={turtleImg} alt="User Image"/>*/}
